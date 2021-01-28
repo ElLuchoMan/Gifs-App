e9223e412ece8eb4ebd5687da1e6b0f1e9911fa1
ElLuchoMan <luchoman96@gmail.com>
2021-01-28 17:15:58 -0500

    Se separa por componentes y módulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { GifsModule } from './gifs/gifs.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    SharedModule,
+    GifsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
