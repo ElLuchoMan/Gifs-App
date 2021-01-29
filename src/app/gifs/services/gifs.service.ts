ba830d1860c7f7858c81b4358f3980decad70a0a
ElLuchoMan <luchoman96@gmail.com>
2021-01-28 21:29:16 -0500

    Guarda en local storag

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -12,19 +12,24 @@ export class GifsService {
   get historial() {
     return [...this._historial];
   }
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    this._historial = JSON.parse(localStorage.getItem('historial')!) || [];
+    this.resultados= JSON.parse(localStorage.getItem('resultados')!) || [];
+
+  }
 
   buscarGifs(query: string = '') {
     query = query.trim().toLowerCase();
     if (!this._historial.includes(query)) {
       this._historial.unshift(query);
       this._historial = this._historial.splice(0, 10);
-        }
+      localStorage.setItem('historial', JSON.stringify(this._historial));
+    }
     this.http.get<SearchGifsResponse>(`https://api.giphy.com/v1/gifs/search?api_key=SRUNpL6UX3UzOjzlnQHi6wWw1EmFSRpx&q=${query}&limit=10`)
       .subscribe((resp) => {
         console.log(resp.data);
         this.resultados = resp.data;
-
+        localStorage.setItem('resultaos', JSON.stringify(this.resultados));
       })
   }
 }
